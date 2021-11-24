ed6fc0e02aa6f5ec1971682d3a4727f295b5cd4a
amangarg97 <amangarg34.ag@gmail.com>
2021-11-24 22:50:19 +0530

    changes scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+  $(document).on('click', '.mobile-nav a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
